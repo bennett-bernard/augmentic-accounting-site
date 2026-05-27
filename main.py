72ef26cb9576f52991e6a7662750e8827ba99239
Bennett Bernard <bennett.s.bernard@gmail.com>
2026-05-27 10:51:24 -0700

    Remove course references

--- main.py
+++ main.py
@@ -20,23 +20,6 @@ mimetypes.add_type("application/javascript", ".js")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
-COURSES = [
-    {
-        "slug": "sql-for-accountants",
-        "title": "SQL for Accountants",
-        "description": (
-            "A practical introduction to SQL for accountants who want to query "
-            "their own data, ask better questions, and work more effectively "
-            "with analysts and engineering teams."
-        ),
-        "thumbnail_path": "images/sql-for-accountants.svg",
-        "checkout_url": "https://gumroad.com/l/sql-for-accountants-placeholder",
-        "cta_label": "Get now",
-    }
-]
-
-
 @app.get("/")
 def home(request: Request):
     articles = []
@@ -76,13 +59,6 @@ def home(request: Request):
 @app.get("/about")
 def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
-
-
-@app.get("/courses")
-def courses(request: Request):
-    return templates.TemplateResponse(
-        "courses.html", {"request": request, "courses": COURSES}
-    )
 
 
 @app.get("/resources")
